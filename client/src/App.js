b72397e853d3030a7d3ed6d94e2098904d4df1b2
agunghanifan <agung.hanifan@gmail.com>
2021-03-29 18:03:18 +0700

    Commit sore, inisiasi commit setting react folder

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  render () {
+    const {  } = this.state
+
+    return (
+      <div>
+        
+      </div>
+    )
+      
+  }
+}
+
+export default App;
