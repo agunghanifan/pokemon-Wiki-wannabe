06d91d7c6ed1a7837402555d1df6d72b2457ae2b
agunghanifan <agung.hanifan@gmail.com>
2021-03-30 11:21:58 +0700

    changes object react to function react

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,51 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardPokemon from './components/Card-pokemon'
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pokemons: [],
-      fetchLink: 'https://pokeapi.co/api/v2/pokemon/'
-    }
+function App () {
+  const [pokemons, setPokemons] = useState([])
+  const [fetchLink, setFetchLink] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const cardDivWidth = {
+    width: '100%'
   }
 
-  shuffleData(event) {
-    event.preventDefault()
+  function shuffleData (e) {
+    e.preventDefault()
     let startById = Math.round(Math.random() * 878)
-    this.setState({
-      ...this.state,
-      fetchLink: `https://pokeapi.co/api/v2/pokemon/?offset=${startById}&limit=20`,
-    })
-    let timing = setInterval(() => {
-      this.getDataFromServer()
-      clearInterval(timing)
-    }, 300);
+    setFetchLink(`https://pokeapi.co/api/v2/pokemon/?offset=${startById}&limit=20`)
   }
 
-  getDataFromServer() {
-    fetch(this.state.fetchLink)
+  useEffect(() => {
+    console.log('masuk effect')
+    fetch(fetchLink)
       .then(res => res.json())
-      .then(res => {
-        // console.log(res.results);
-        this.setState({
-          ...this.state,
-          pokemons: res.results
-        })
+      .then(pokemonsData => {
+        console.log(pokemonsData)
+        setPokemons(pokemonsData.results)
       })
       .catch(err => {
         console.log(err)
       })
-  }
+  }, [fetchLink, setPokemons])
 
-  componentDidMount() {
-    this.getDataFromServer()
-  }
 
-  render () {
-    const { pokemons } = this.state
-    const cardDivWidth = {
-      width: '100%'
-    }
-    return (
-      <div className="container">
-        <h1 className="text-center ml-5 header-font" >Worlds of<img className="img-header" src="pngaaa.com-14402(1).png" alt='pokemon'></img></h1>
-        <div className="mt-3 mb-5 row mx-auto d-flex justify-content-center" style={cardDivWidth}>
-          {
-            pokemons.map((pokemon) => {
-              // return <li>{pokemon.name} - {pokemon.url}</li>
-              return <CardPokemon pokemon={pokemon} key={pokemon.name}></CardPokemon>
-            })
-          }
-          <div className="justify-content-center d-flex">
-            <button className="btn btn-warning mt-5 btn-text" onClick={(event) => this.shuffleData(event)}>Lets Shuffle this boxes</button>
-          </div>
+  return (
+    <div className="container">
+      <h1 className="text-center ml-5 header-font" >Worlds of<img className="img-header" src="pngaaa.com-14402(1).png" alt='pokemon'></img></h1>
+      <div className="mt-3 mb-5 row mx-auto d-flex justify-content-center" style={cardDivWidth}>
+        {
+          pokemons.map((pokemon) => {
+            return <CardPokemon pokemon={pokemon} key={pokemon.name}></CardPokemon>
+          })
+        }
+        <div className="justify-content-center d-flex">
+          <button className="btn btn-warning mt-5 btn-text" onClick={(e) => shuffleData(e)}>Lets Shuffle this boxes</button>
         </div>
       </div>
-    )
-      
-  }
+    </div>
+  )
 }
 
 export default App;
--- client/src/components/Card-pokemon.jsx
+++ client/src/components/Card-pokemon.jsx
@@ -1,72 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 import './Card-pokemon.css'
 
-class CardPokemon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemons_img: '',
-      changesPageStatus: false,
-    }
+
+function CardPokemon (props) {
+  const [ pokemonsImg, setPokemonsImg ] = useState('')
+  const { pokemon } = props
+  const cardImage = {
+    width: '150px',
+    height: '150px',
+  }
+  const cardWidth = {
+    width: '18rem',
   }
 
-  getImageFromServer() {
-    const { pokemon } = this.props
+  useEffect(() => {
     fetch(pokemon.url)
       .then(res => res.json())
-      .then(res => {
-        // console.log(res)
-        this.setState({
-          ...this.state,
-          pokemons_img: res.sprites.other['official-artwork'].front_default,
-        })
+      .then(pokemonData => {
+        setPokemonsImg(pokemonData.sprites.other['official-artwork'].front_default)
       })
-  }
+  }, [pokemon, setPokemonsImg])
 
-  componentDidMount() {
-    this.getImageFromServer()
-  }
-
-  changesPage(event) {
-    event.preventDefault();
-    this.setState({
-      ...this.state,
-      changesPageStatus: true,
-    })
-    
-    let timing = setInterval(() => {
-      console.log(this.state.changesPageStatus)
-      clearInterval(timing)
-    }, 500);
-  }
-
-  render() {
-    const { pokemon } = this.props
-    const cardImage = {
-      width: '150px',
-      height: '150px',
-    }
-    const cardWidth = {
-      width: '18rem',
-    }
-    return (
-      <>
-        <Card className="bg-image hover-zoom card col-2 m-2 border-0 shadow" style={cardWidth}>
-          <CardImg className="w-100" top width="100%" src={this.state.pokemons_img} alt="Card image cap" style={cardImage} />
-          <CardBody>
-            <CardTitle tag="h5">{pokemon.name}</CardTitle>
-            <div>
-              <Button className="btn-text" onClick={(event) => this.changesPage(event)}>Details..</Button>
-            </div>
-          </CardBody>
-        </Card>
-      </>
-      )
-  }
+  return (
+    <>
+      <Card className="bg-image hover-zoom card col-2 m-2 border-0 shadow" style={cardWidth}>
+        <CardImg className="w-100" top width="100%" src={pokemonsImg} alt="Card image cap" style={cardImage} />
+        <CardBody>
+          <CardTitle tag="h5">{pokemon.name}</CardTitle>
+          <div>
+            <Button className="btn-text" onClick={(event) => this.changesPage(event)}>Details..</Button>
+          </div>
+        </CardBody>
+      </Card>
+    </>
+    )
 }
 
 export default CardPokemon;
